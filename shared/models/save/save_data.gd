6ff42e6ac350bf8d698cf61bea3bf1e967ca5be8
akrck02 <aketza.vazquez@gmail.com>
2024-12-29 20:27:38 +0100

    refactor: save data

--- shared/models/save/save_data.gd
+++ shared/models/save/save_data.gd
@@ -5,6 +5,7 @@ var quests : Dictionary = {}
 var inventory : Dictionary = {}
 var minigames : Dictionary = {}
 
+
 ## Set pet
 func set_pet(pet : Dictionary) -> void:
 	pets[pet.name] = pet
@@ -17,28 +18,67 @@ func pet_exists(name : String) -> bool:
 
 ## Get pet
 func get_pet(name : String) -> Dictionary:
+	if not pet_exists(name):
+		printerr("Pet %s does not exists." % name)
+		return {}
+		
 	return pets[name]
 
 
-func set_quest() -> void:
-	pass
+## Set quest
+func set_quest(quest : Dictionary) -> void:
+	quests[quest.id] = quest
 
-func set_item() -> void:
-	pass
 
-func remove_item() -> void:
-	pass
+## Get if quest exists
+func quest_exists(id : int) -> bool:
+	return quests.has(id)
+
+
+## Get quest
+func get_quest(id : int) -> Dictionary:
+	if not quest_exists(id):
+		printerr("Quest %s does not exists." % id)
+		return {}
+	
+	return quests[id]
+
+
+## Set item 
+func set_item(item : Dictionary) -> void:
+	inventory[item.id] = item
+
+
+## Get if item exists
+func item_exists(id : int) -> bool:
+	return inventory.has(id)
+
+
+## Get item 
+func get_item(id: int) -> Dictionary:
+	if not item_exists(id):
+		printerr("Item %s does not exists." % id)
+		return {}
+	
+	return inventory[id]
+
+
+## Remove item
+func remove_item(id : int) -> void:
+	if not item_exists(id):
+		printerr("Item %s does not exists." % id)
+		return
+	
+	inventory.erase(id)
 
 
 ## Returns a SaveData object serialized as a String in json format.
 func _to_string() -> String:
-	var data = {
-		"pets" : pets,
-		"quests" : quests,
-		"inventory" : inventory,
-		"minigames" : minigames
-	}
-	
+	var data = {}
+	data.pets = pets
+	data.quests = quests
+	data.inventory = inventory
+	data.minigames = minigames
 	return JSON.stringify(data,"    ")
 
 
@@ -51,15 +91,16 @@ static func from_json(json : String) -> SaveData:
 	if null == parsed_data:
 		return save_data
 
-	for pet in parsed_data["pets"]:
-		save_data.set_pet(parsed_data["pets"][pet])
+	var parsed_pets = parsed_data["pets"]
+	for pet_name in parsed_pets:
+		save_data.set_pet(parsed_pets[pet_name])
 
-	for quest in parsed_data["quests"]:
-		print(quest)
-		save_data.set_quest()
+	var parsed_quests = parsed_data["quests"]
+	for quest_name in parsed_quests:
+		save_data.set_quest(parsed_quests[quest_name])
 
-	for item in parsed_data["inventory"]:
-		print(item)
-		save_data.set_item()
+	var parsed_inventory = parsed_data["inventory"]
+	for item_name in parsed_inventory:
+		save_data.set_item(parsed_inventory[item_name])
 
 	return save_data
