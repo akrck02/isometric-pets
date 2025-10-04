170cbf8bb8f8b5b37d448cf3a7183db025f2d171
tekofx <tekofxx@gmail.com>
2025-10-04 14:42:56 +0200

    Created inventory, food and oniguiri

---
+++ old/entities/items/nodes/food/food.gd
@@ -0,0 +1,6 @@
+extends Item
+class_name Food
+
+
+func _ready() -> void:
+	item_type=ItemType.FOOD
--- old/entities/items/nodes/item/item.gd
+++ old/entities/items/nodes/item/item.gd
@@ -1,9 +1,9 @@
 class_name Item
-extends Node
+extends Resource
+
 
 
 enum ItemType {FOOD, PLANT, FURNITURE}
-
 @export var item_type: ItemType
 @export var item_name: String
 @export var item_description: String
