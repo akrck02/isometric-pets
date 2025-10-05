6e971261ef2e01644a8d3ed50ee5ba95ffe4d828
tekofx <tekofxx@gmail.com>
2025-10-05 10:19:58 +0200

    Fixed sprite not appearing in inventory slot

--- old/ui/hud/apps/inventory/inv_ui_slot.gd
+++ old/ui/hud/apps/inventory/inv_ui_slot.gd
@@ -11,9 +11,7 @@ var item_quantity:int=0
 func update(item:Item, quantity:int):
 	if !slot_item:
 		slot_item=item
-		print(item.item_name)
-		print(item.item_description)
-		#sprite_2d.texture=item.item_texture
-		#sprite_2d.visible=true
+		sprite_2d.texture=item.item_texture
+		sprite_2d.visible=true
 		label.text=str(quantity)
 		label.visible=true
