426b17d18492e1c395f668b6343df455a96fe58c
tekofx <tekofxx@gmail.com>
2025-10-07 17:36:48 +0200

    Hide or show prev and next buttons in inventory

--- old/ui/hud/apps/inventory/inventory.gd
+++ old/ui/hud/apps/inventory/inventory.gd
@@ -8,7 +8,8 @@ extends App
 var onigiri = preload("res://entities/items/nodes/food/oniguiri.tres")   
 
 const ITEMS_PER_PAGE = 20
-var current_page = 1
+var current_page = 0
+var last_page = 1
 
 
 
@@ -17,6 +18,8 @@ func _ready() -> void:
 	prev_page_button.pressed.connect(_prev_page)
 	for i in range(50):
 		items.append(onigiri.duplicate())
+		
+	last_page = ceil(float(items.size()) / ITEMS_PER_PAGE) - 1
 	
 	
 
@@ -24,16 +27,30 @@ func _get_current_page_items():
 	var start = current_page * ITEMS_PER_PAGE
 	var end = min(start + ITEMS_PER_PAGE, items.size())
 	return items.slice(start, end)
+	
+func _show_or_hide_buttons():
+	if current_page==0:
+		next_page_button.visible=true
+		prev_page_button.visible=false
+	elif current_page==last_page:
+		next_page_button.visible=false
+		prev_page_button.visible=true
+	else:
+		next_page_button.visible=true
+		prev_page_button.visible=true
 
 func _next_page():
 	if (current_page + 1) * ITEMS_PER_PAGE < items.size():
 		current_page += 1
+		
+	_show_or_hide_buttons()
 	_update_slots()
 
 func _prev_page():
 	if current_page > 0:
 		current_page -= 1
 		
+	_show_or_hide_buttons()
 	_update_slots()
 			
 func _update_slots():
@@ -51,4 +68,3 @@ func _empty_slots():
 func open() -> void:
 	super()
 	_update_slots()
-		
