d780be2dce5f88e62a1a99955828ddf592ab0c5f
tekofx <tekofxx@gmail.com>
2025-10-07 17:23:08 +0200

    Implemented basic pagination of inventory

--- old/ui/hud/apps/inventory/inv_ui_slot.gd
+++ old/ui/hud/apps/inventory/inv_ui_slot.gd
@@ -9,9 +9,15 @@ var item_quantity:int=0
 
 
 func update(item:Item, quantity:int):
-	if !slot_item:
-		slot_item=item
-		sprite_2d.texture=item.item_texture
-		sprite_2d.visible=true
-		label.text=str(quantity)
-		label.visible=true
+	slot_item=item
+	sprite_2d.texture=item.item_texture
+	sprite_2d.visible=true
+	label.text=str(quantity)
+	label.visible=true
+
+func empty():
+	slot_item=null
+	sprite_2d.texture=null
+	sprite_2d.visible=false
+	label.text="0"
+	label.visible=false
--- old/ui/hud/apps/inventory/inventory.gd
+++ old/ui/hud/apps/inventory/inventory.gd
@@ -3,14 +3,52 @@ extends App
 @export var items: Array[Item]
 @onready var grid_container: GridContainer = $GridContainer
 @onready var slots: Array = $VBoxContainer/GridContainer.get_children()
+@onready var next_page_button: Button = $VBoxContainer/HBoxContainer/NextPageButton
+@onready var prev_page_button: Button = $VBoxContainer/HBoxContainer/PrevPageButton
+var onigiri = preload("res://entities/items/nodes/food/oniguiri.tres")   
 
+const ITEMS_PER_PAGE = 20
+var current_page = 1
+
+
+
+func _ready() -> void:
+	next_page_button.pressed.connect(next_page)
+	prev_page_button.pressed.connect(prev_page)
+	for i in range(50):
+		items.append(onigiri.duplicate())
+	
+	
+
+func get_current_page_items():
+	var start = current_page * ITEMS_PER_PAGE
+	var end = min(start + ITEMS_PER_PAGE, items.size())
+	return items.slice(start, end)
+
+func next_page():
+	if (current_page + 1) * ITEMS_PER_PAGE < items.size():
+		current_page += 1
+	_update_slots()
+
+func prev_page():
+	if current_page > 0:
+		current_page -= 1
+		
+	_update_slots()
+			
+func _update_slots():
+	_empty_slots()
+	var slot_index= 0
+	for item in get_current_page_items():
+		if slot_index<20:
+			slots[slot_index].update(item,10*current_page+slot_index)
+			slot_index+=1
+			
+func _empty_slots():
+	for slot in slots:
+		slot.empty()
 
 func open() -> void:
 	super()
-	var index= 3
-	for item in items:
-		for slot in slots:
-			if !slot.slot_item:
-				slot.update(item, index)
-				index+=1
-				break
+	_update_slots()
+		
